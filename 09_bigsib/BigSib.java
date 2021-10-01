1c8e03c0c2111d480ba4217d6b9571d4bed7878b
Nakib Abedin <nabedin30@stuy.edu>
2021-10-01 19:45:17 -0400

    HW09

---
+++ 09_bigsib/BigSib.java
@@ -0,0 +1,11 @@
+public class BigSib {
+  public static void main (String [] args){
+        greet("Nakib");
+        greet("Alif");
+        greet("Lior");
+        }
+  public static void greet(String name){
+        System.out.println("Why, hello there, " + name + ". How do you do?");
+        }
+}
+
