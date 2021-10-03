0e2b4707b14305ecf0dae3175146ff887f146d84
Nakib Abedin <nabedin30@stuy.edu>
2021-10-03 19:42:08 -0400

    HW09 Upadate 2.0

--- 09_bigsib/BigSib.java
+++ 09_bigsib/BigSib.java
@@ -1,9 +1,11 @@
+/*
+TNPG:SleepDeprivedBois | Roster: Nakib Abedin, Joker (duckie)
+APCS
+HW09 -- What Are BigSibs Good For?
+2021-10-03
+*/
+
 public class BigSib {
-  public static void main (String [] args){
-        greet("Nakib");
-        greet("Alif");
-        greet("Lior");
-        }
   public static void greet(String name){
         System.out.println("Why, hello there, " + name + ". How do you do?");
         }
--- 09_bigsib/Greet.java
+++ 09_bigsib/Greet.java
@@ -1,27 +1,26 @@
 /*
- TNPG: LAN | Nakib Abedin, Alif Rahman, Lior Polischouk
- APCS
- HW08 -- Refactor Freshie Zero
- 2021-09-30
+TNPG:SleepDeprivedBois | Roster: Nakib Abedin, Joker (duckie)
+APCS
+HW09 -- What Are BigSibs Good For?
+2021-10-03
 
-Discoveries:
-1) You can call a method within another method.
-2) It seems like you need to have an argument to call a method within another method. It seems like you can't leave the argument section blank.
-3) From our obserations and experimentation, it seems like you can not define a method within another method. To have methodY inside methodX methodY needs to be defined in the same class and then it can be called inside method X
 
-Unresolved Questions:
-1) Do you need to have arguments when calling a function in Java? If not, why did it return an error when we tried to call greet() without and argument?
-2) Is it possible to define a method within a method?
+DISCOVERIES
+1) You can call methods from another file as long as the file is in the same directory.
+2) You don't need to have a main method if you aren't planning to execute the code directly. The functions can still be called in a different file and executed accordingly.
+3) To call a function from a certain file, the format is <className>.<methodName>
+UNRESOLVED QUESTIONS
+1) No unresolved mysteries.
+
+
+
 */
 
 public class Greet {
   public static void main (String [] args){
 	BigSib.greet("Nakib");
-	BigSib.greet("Alif");
-	BigSib.greet("Lior");
-	}
-  public static void greet(String name){
-	System.out.println("Why, hello there, " + name + ". How do you do?");
+	BigSib.greet("Joker");
+	BigSib.greet("Batman");
 	}
 }
 
