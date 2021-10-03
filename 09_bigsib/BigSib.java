5ac811659f78275e436c6d221dd77d47ce366a43
Nakib Abedin <76085243+nakibabedin@users.noreply.github.com>
2021-10-03 19:43:00 -0400

    Update BigSib.java

--- 09_bigsib/BigSib.java
+++ 09_bigsib/BigSib.java
@@ -3,6 +3,13 @@ TNPG:SleepDeprivedBois | Roster: Nakib Abedin, Joker (duckie)
 APCS
 HW09 -- What Are BigSibs Good For?
 2021-10-03
+
+DISCOVERIES
+1) You can call methods from another file as long as the file is in the same directory.
+2) You don't need to have a main method if you aren't planning to execute the code directly. The functions can still be called in a different file and executed accordingly.
+3) To call a function from a certain file, the format is <className>.<methodName>
+UNRESOLVED QUESTIONS
+1) No unresolved mysteries.
 */
 
 public class BigSib {
