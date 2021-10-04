b818b738dd8c1e69a5ae674fa72a0a60ae8aa6a0
Nakib Abedin <76085243+nakibabedin@users.noreply.github.com>
2021-10-04 19:57:59 -0400

    HW10

---
+++ 10_bigsib/BigSib.java
@@ -0,0 +1,24 @@
+/*
+TNPG: Luminous Units | Roster: Nakib Abedin, Joker (duckie)
+APCS
+HW10 -- Refactor Big Sib One
+2021-10-04
+
+
+DISCOVERIES
+1) For Java methods, you need to specify the type of value that the method will$
+2) There is also a return statement in java.                                  
+3)Trace diagrams are helpful when we can calling methods inside of other method$
+
+UNRESOLVED QUESTIONS
+1) No unresolved Questions.
+
+*/
+
+public class BigSib {
+  public static String greet(String name){
+        String value = ("Why, hello there, " + name + ". How do you do?");
+        return(value);
+	}
+}
+
---
+++ 10_bigsib/Greet.java
@@ -0,0 +1,26 @@
+/*
+TNPG:SleepDeprivedBois | Roster: Nakib Abedin, Joker (duckie)
+APCS
+HW10 -- Refactor Big Sib One
+2021-10-04
+
+
+DISCOVERIES
+1) For Java methods, you need to specify the type of value that the method will return while defining it. This is done after writing "public static" (E.g. public static int).
+2) There is also a return statement in java.
+3)Trace diagrams are helpful when we can calling methods inside of other methods such as how we called greet from BigSib.class inside of SOP.
+
+UNRESOLVED QUESTIONS
+1) No unresolved Questions.
+
+
+*/
+
+public class Greet {
+  public static void main (String [] args){
+	System.out.println(BigSib.greet("Crosby"));
+	System.out.println(BigSib.greet("Stills"));
+	System.out.println(BigSib.greet("Nash"));
+	}
+}
+
