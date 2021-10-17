6444132bd7496081f7178b3fc7909e8f6cbda229
Nakib Abedin <76085243+nakibabedin@users.noreply.github.com>
2021-10-17 17:12:42 -0400

    HW12 in progress

--- 18_BankAccount/BankAccount.java
+++ 18_BankAccount/BankAccount.java
@@ -3,36 +3,59 @@
 */
 
 public class BankAccount {
-	String Fullname;
-	String Password;
-	int Pin;
-	int AccountNumber;
-	double AccountBalance;
+	String fullName;
+	String password;
+	int pin;
+	int accountNumber;
+	double accountBalance;
 
 	public BankAccount() {
-
 	}
+	
+	public BankAccount(String FullName, String Password, int Pin, int AccountNumber, double AccountBalance){
+		String fullName = FullName;
+        	String password = Password;
+        	int pin = Pin;
+        	int accountNumber = AccountNumber;
+        	double accountBalance = AccountBalance;
+	}
+	
 	public void setName(String name) {
-		Fullname = name;
+		fullName = name;
 	}
 	public void setPassword(String pass) {
-		Password = pass;
+		password = pass;
 	}
-	public void setPin(int pin) {
-		Pin = pin;
+	public void setPin(int pinNo) {
+		pin = pinNo;
 	}
 	public void setAccountNumber(int actno) {
-		AccountNumber = actno;
+		accountNumber = actno;
 	}
 	public void setAccountBalance(double bal) {
-		AccountBalance = bal;
+		accountBalance = bal;
 	}
-	public void printAll (String FullName, String Password, int Pin, int AccountNumber, int AccountBalance) {
-	System.out.println(FullName);
-	System.out.println(Password);
-	System.out.println(Pin);
-	System.out.println(AccountNumber);
-	System.out.println(AccountBalance);
+
+
+	public static void printAll (String FullName, String Password, int Pin, int AccountNumber, double AccountBalance) {
+		System.out.println(FullName);
+		System.out.println(Password);
+		System.out.println(Pin);
+		System.out.println(AccountNumber);
+		System.out.println(AccountBalance);
+	}
+
+	public void printAllnotVoid() {
+                System.out.println(fullName);
+                System.out.println(password);
+                System.out.println(pin);
+                System.out.println(accountNumber);
+                System.out.println(accountBalance);
+        }
+
+	public static void main (String[] args){
+		printAll("ol Nakib", "NotPassword", 42069, 123456, 532.00);
+		BankAccount nakib = new BankAccount();
 	}
 }
 
