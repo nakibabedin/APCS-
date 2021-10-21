9960d841b2aec07887b201c051c11dd6a7b6fb08
Nakib Abedin <76085243+nakibabedin@users.noreply.github.com>
2021-10-21 08:38:01 -0400

    updated HW21

--- 21_StatsLol/Stats.java
+++ 21_StatsLol/Stats.java
@@ -1,15 +1,23 @@
 import java.lang.Math;
 
-/**
-   TNPG: NAW FAM -- ALIF RAHMAN, NAKIB ABEDIN, JOKER (ducky)
+/*
+   TNPG: NAW FAM -- ALIF RAHMAN, GRIPPY (ducky), NAKIB ABEDIN, JOKER (ducky)
    APCS
-   HW20 --
+   HW21 -- STAtisTically Speaking...
    2021-10-21
    TASK:
    Implement each method without typecasting.
    xtra: harmonic mean is the reciprocal of the
    arithmetic mean of the reciprocals
- **/
+   
+   DISCO
+   - Java automatically floor divides when dividing two ints. i.e. 3/2 -> 1
+   - Typecasting changes the type of the literal.
+   QCC
+   - We didn't see a way to calculate geoMean() without using the class Math. 
+   - How would you convert the sqrt (which is a double) into an int without typecasting?
+   
+ */
 
 public class Stats {
 
@@ -43,11 +51,11 @@ public class Stats {
 
   public static int geoMean(int a, int b) {
 	int product = a * b;
-	int gmean = Math.pow(product);
+	int gmean = (int) Math.sqrt(product);
 	return gmean;
   }
 
-/*
+
   public static double geoMean(double a, double b) {
 	double product = a * b;
 	double gmean = Math.sqrt(product);
@@ -64,7 +72,6 @@ public class Stats {
 			return c;
 		}
   }
-
   public static double max(double a, double b, double c) {
 	if (a > c & a > b){
                 return a;
@@ -75,27 +82,21 @@ public class Stats {
                         return c;
                 }
   }
-
   public static int geoMean(int a, int b, int c) {
 	int product = a * b * c;
-	int gmean = Math.sqrt(product);
+	int gmean = (int) Math.sqrt(product);
 	return gmean;
 	
   }
-
   public static double geoMean(double a, double b, double c) {
 	double product = a * b * c;
         double gmean = Math.sqrt(product);
         return gmean;
   }
-
-
   //main method for testing functionality
   public static void main( String[] args ) {
-	System.out.print("StatsLol");
+	System.out.print("Stats");
   }
-*/
+
 
 }//end class
-
-
--- 21_StatsLol/StatsDriver.java
+++ 21_StatsLol/StatsDriver.java
@@ -1,6 +1,21 @@
 /*
-
-*/
+   TNPG: NAW FAM -- ALIF RAHMAN, GRIPPY (ducky), NAKIB ABEDIN, JOKER (ducky)
+   APCS
+   HW21 -- STAtisTically Speaking...
+   2021-10-21
+   TASK:
+   Implement each method without typecasting.
+   xtra: harmonic mean is the reciprocal of the
+   arithmetic mean of the reciprocals
+   
+   DISCO
+   - Java automatically floor divides when dividing two ints. i.e. 3/2 -> 1
+   - Typecasting changes the type of the literal.
+   QCC
+   - We didn't see a way to calculate geoMean() without using the class Math. 
+   - How would you convert the sqrt (which is a double) into an int without typecasting?
+   
+ */
 
 public class StatsDriver {
 	public static void main (String[] args){
@@ -18,8 +33,8 @@ public class StatsDriver {
 		System.out.println("expecting 0");
 		System.out.println(Stats.max(2.0, 3.0));
 		System.out.println("expecting 3.0");
-		System.out.println(Stats.max(2, 3.2));
-		System.out.println("expecting error because max method expects two integer literals or two double literals");
+		//System.out.println(Stats.max(2, 3.2));
+		//System.out.println("expecting error because max method expects two integer literals or two double literals");
 		System.out.println(Stats.geoMean(4, 1));
 		System.out.println("expecting 2");
 		System.out.println(Stats.geoMean(4.0,1.0));
