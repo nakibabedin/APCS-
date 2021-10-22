ced2ca7dc7a6865df6350f91b42f83095146231d
Nakib Abedin <76085243+nakibabedin@users.noreply.github.com>
2021-10-22 11:15:47 -0400

    21_Stats Improved

--- 21_Stats/Stats.java
+++ 21_Stats/Stats.java
@@ -1,5 +1,3 @@
-import java.lang.Math;
-
 /*
    TNPG: NAW FAM -- ALIF RAHMAN, GRIPPY (ducky), NAKIB ABEDIN, JOKER (ducky)
    APCS
@@ -9,59 +7,65 @@ import java.lang.Math;
    Implement each method without typecasting.
    xtra: harmonic mean is the reciprocal of the
    arithmetic mean of the reciprocals
-   
    DISCO
    - Java automatically floor divides when dividing two ints. i.e. 3/2 -> 1
    - Typecasting changes the type of the literal.
    QCC
-   - We didn't see a way to calculate geoMean() without using the class Math. 
+   - We didn't see a way to calculate geoMean() without using the class Math.
    - How would you convert the sqrt (which is a double) into an int without typecasting?
-   
  */
 
 public class Stats {
 
   public static int mean(int a, int b) {
-	int sum = a + b;
-	int mean = sum/2;
-	return mean;
+	   int sum = a + b;
+	   int mean = sum/2;
+	   return mean;
   }
 
   public static double mean(double a, double b) {
-	double sum = a + b;
-	double mean = sum/2;
-	return mean;
+	   double sum = a + b;
+	   double mean = sum/2;
+	   return mean;
   }
 
   public static int max(int a, int b) {
-	if (a > b){ 
-		return a;
+    if (a > b) {
+       return a;
 	}else{
-		return b;
+		   return b;
 	}
   }
 
   public static double max(double a, double b) {
-	if (a > b){
-		return a;
-	}else{
-		return b;
+	   if (a > b){
+       return a;
+	   }else{
+       return b;
 	}
   }
 
   public static int geoMean(int a, int b) {
-	int product = a * b;
-	int gmean = (int) Math.sqrt(product);
-	return gmean;
+    int answer = 0;
+    int product = a*b;
+    double gmean = Math.sqrt(product);
+    while ((answer) < gmean) { // basically saying if answer is less than the geometric mean, add one. When the loop finishes the answer will be equal to either exactly gmean or rounded up gmean.
+      answer += 1;
+    }
+    if ((answer) == gmean) { // if the answer is equal to gmean, just return answer
+      return answer;
+    }
+    else { // if this code runs, that means gmean is not a whole number. Therefore, the while loop would have produced an answer that is a rounded up gmean. We want to floor. Therefore, subtract 1.
+      return answer - 1;
+    }
   }
-
 
   public static double geoMean(double a, double b) {
 	double product = a * b;
 	double gmean = Math.sqrt(product);
 	return gmean;
   }
- 
+
   public static int max(int a, int b, int c) {
 	if (a > c & a > b){
 		return a;
@@ -72,6 +76,7 @@ public class Stats {
 			return c;
 		}
   }
+
   public static double max(double a, double b, double c) {
 	if (a > c & a > b){
                 return a;
@@ -82,12 +87,22 @@ public class Stats {
                         return c;
                 }
   }
+
   public static int geoMean(int a, int b, int c) {
-	int product = a * b * c;
-	int gmean = (int) Math.sqrt(product);
-	return gmean;
-	
+    int answer = 0;
+    int product = a*b*c;
+    double gmean = Math.sqrt(product);
+    while ((answer) < gmean) {
+      answer += 1;
+    }
+    if ((answer) == gmean) {
+      return answer;
+    }
+    else {
+      return answer - 1;
+    }
   }
+
   public static double geoMean(double a, double b, double c) {
 	double product = a * b * c;
         double gmean = Math.sqrt(product);
--- 21_Stats/StatsDriver.java
+++ 21_Stats/StatsDriver.java
@@ -7,37 +7,33 @@
    Implement each method without typecasting.
    xtra: harmonic mean is the reciprocal of the
    arithmetic mean of the reciprocals
-   
    DISCO
    - Java automatically floor divides when dividing two ints. i.e. 3/2 -> 1
    - Typecasting changes the type of the literal.
    QCC
-   - We didn't see a way to calculate geoMean() without using the class Math. 
+   - We didn't see a way to calculate geoMean() without using the class Math.
    - How would you convert the sqrt (which is a double) into an int without typecasting?
-   
  */
 
 public class StatsDriver {
 	public static void main (String[] args){
-		System.out.println(Stats.mean(6, 4));
-		System.out.println("expecting 5");
-		System.out.println(Stats.mean(6, 6));
-		System.out.println("6");
-		System.out.println(Stats.mean(12.0, 15.0));
-		System.out.println("expecting 13.5");
+		System.out.println(Stats.mean(6, 4) + " | 5");
+		System.out.println(Stats.mean(6, 6) + " | 6");
+		System.out.println(Stats.mean(12.0, 15.0) + " | 13.5");
 		// System.out.println(Stats.mean());
 		// System.out.println("expecting an error; the method expects two integer literals");
-		System.out.println(Stats.max(2,3));
-		System.out.println("expecting 3");
-		System.out.println(Stats.max(0,0));
-		System.out.println("expecting 0");
-		System.out.println(Stats.max(2.0, 3.0));
-		System.out.println("expecting 3.0");
+		System.out.println(Stats.max(2,3) + " | 3");
+		System.out.println(Stats.max(0,0) + " | 0");
+		System.out.println(Stats.max(2.0, 3.0) + " | 3.0");
 		//System.out.println(Stats.max(2, 3.2));
 		//System.out.println("expecting error because max method expects two integer literals or two double literals");
-		System.out.println(Stats.geoMean(4, 1));
-		System.out.println("expecting 2");
-		System.out.println(Stats.geoMean(4.0,1.0));
-		System.out.println("expecting 2.0");
+    System.out.println(Stats.geoMean(4, 1) + " | 2");
+		System.out.println(Stats.geoMean(4.0,1.0) + " | 2.0");
+		System.out.println(Stats.geoMean(6, 1) + " | 2");
+		System.out.println(Stats.geoMean(6.0,1.0) + " | 2.4494...");
+    System.out.println(Stats.geoMean(4, 1, 9) + " | 6");
+    System.out.println(Stats.geoMean(4.0, 1.0, 9.0) + " | 6.0");
+    System.out.println(Stats.geoMean(6, 1, 7) + " | 6");
+    System.out.println(Stats.geoMean(6.0, 1.0, 7.0) + " | 6.4807...");
 	}
 }
