961be1a56180e176e009cc15f3bdb3d077ec0132
Nakib Abedin <nabedin30@stuy.edu>
2021-10-31 21:38:06 -0400

    cb added

---
+++ cb/string-1/String.java
@@ -0,0 +1,9 @@
+public class String{
+  public String helloName(String name) {
+  return "Hello " + name + "!";
+  }
+
+  public String makeAbba(String a, String b) {
+  return a + b + b + a;
+  }  
+}
