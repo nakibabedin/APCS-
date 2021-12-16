4fb27d772270b8037b34ac0dcf843b24423f3982
nakibabedin <nabedin30@stuy.edu>
2021-12-15 21:02:50 -0500

    update BinSearch.java

--- 48_search/BinSearch.java
+++ 48_search/BinSearch.java
@@ -7,7 +7,7 @@ public class BinSearch
 {
 
   /**
-     int binSearch(Comparable[],Comparable) -- searches an array of 
+     int binSearch(Comparable[],Comparable) -- searches an array of
      Comparables for target Comparable
      pre:  input array is sorted in ascending order
      post: returns index of target, or returns -1 if target not found
@@ -44,15 +44,29 @@ public class BinSearch
     int tPos = -1; //init return var to flag value -1
     int m = (lo + hi) / 2; //init mid pos var
 
-    while( /* ? */ ) { // run until lo & hi cross
+    while( hi > lo ) { // run until lo & hi cross
 
       //update mid pos var
 
+      m = (lo + hi)/2;
+
       // target found
+
+      if (a[m].compareTo(target)==0){
+        tPos = m;
+        return tPos;
+      }
 
       // value at mid index higher than target
 
+      if (a[m].compareTo(target) > 0){
+        hi = m - 1;
+      }
+
       // value at mid index lower than target
+      if (a[m].compareTo(target) < 0){
+        lo = m + 1;
+      }
 
     }
     return tPos;
@@ -67,6 +81,7 @@ public class BinSearch
     boolean retBoo = true; //init to true, assume array is sorted
 
     //Q: Why would a FOREACH loop not suffice here?
+    //A: A FOREACH loop does not come with an in-bulit system for indexing. We can use it, but it opens up room for errors.
     for( int i=0; i < arr.length-1; i++ ) {
       if ( ( arr[i].compareTo(arr[i+1]) > 0 ) ) {
         return false;
@@ -78,7 +93,7 @@ public class BinSearch
 
   // utility/helper fxn to display contents of an array of Objects
   private static void printArray( Comparable[] arr ) {
-    String output = "[ "; 
+    String output = "[ ";
 
     for( Comparable c : arr )
 	    output += c + ", ";
@@ -94,8 +109,6 @@ public class BinSearch
   //minimal -- augment as necessary
   public static void main ( String[] args )
   {
-
-    /*----------------------------------------------------
 
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     System.out.println("\nNow testing binSearch on Comparable array...");
@@ -114,10 +127,10 @@ public class BinSearch
     iArr3[i] = i * 2;
     }
 
-    printArray( iArr3 );
-    System.out.println( "iArr3 sorted? -- " + isSorted(iArr2) );
+    //printArray( iArr3 );
+    //System.out.println( "iArr3 sorted? -- " + isSorted(iArr3) );
 
-    //search for 6 in array 
+    //search for 6 in array
     System.out.println( binSearch(iArr2,2) );
     System.out.println( binSearch(iArr2,4) );
     System.out.println( binSearch(iArr2,6) );
@@ -125,16 +138,19 @@ public class BinSearch
     System.out.println( binSearch(iArr2,13) );
     System.out.println( binSearch(iArr2,42) );
 
-    //search for 43 in array 
-    System.out.println( binSearch(iArr2,43) );
+    //search for 43 in array
+    //System.out.println( binSearch(iArr2,43) );
 
     System.out.println( "now testing binSearch on iArr3..." );
     System.out.println( binSearch(iArr3,4) );
     System.out.println( binSearch(iArr3,8) );
     System.out.println( binSearch(iArr3,5) );
 
-    //search for 43 in array 
+    //search for 43 in array
     System.out.println( binSearch(iArr3,43) );
+
+    /*----------------------------------------------------
+
     ====================================================*/
 
 
