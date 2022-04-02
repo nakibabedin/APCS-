5284f8350ce376cd5d2a2559f1758207cceb2e19
nakibabedin <nabedin30@stuy.edu>
2022-04-01 20:13:51 -0400

    add hw87

--- 87_queue/NodeQueue.java
+++ 87_queue/NodeQueue.java
@@ -1,42 +1,76 @@
-public class NodeQueue<QUASAR> implements Queue<QUASAR> {
+public class NodeQueue<QUASAR> implements Queue<QUASAR>{
 
-    LLNode<QUASAR> _front;
+QNode<QUASAR> _front;
+QNode<QUASAR> _tail;
+int _size = 0;
 
+public NodeQueue(){
+    _front = null;
+    _size = 0;
+}
   //~~~~~~~~~~~~~~~~~~begin AP subset~~~~~~~~~~~~~~~~~~
   //means of removing an element from collection:
   //Dequeues and returns the first element of the queue.
-  public QUASAR dequeue();
+  public QUASAR dequeue(){
+    QNode<QUASAR> tmp = _front;
+    QUASAR retVal;
+    for(int i = 0; i < _size-2; i++){
+        tmp = tmp.getNext();
+    }
+    retVal = tmp.getNext().getCargo();
+    tmp.setNext(null);
+    _size--;
+    return retVal;
+  }
 
   //means of adding an element to collection:
   //Enqueue an element onto the back of this queue.
-  public void enqueue( QUASAR x );
+  public void enqueue( QUASAR x ){
+      QNode<QUASAR> tmp = new QNode<QUASAR>(x, _front);
+      _front = tmp;
+      _size++;
+  }
 
   //Returns true if this queue is empty, otherwise returns false.
-  public boolean isEmpty();
+  public boolean isEmpty(){
+      return _size == 0;
+  }
 
   //Returns the first element of the queue without dequeuing it.
-  public QUASAR peekFront();
+  public QUASAR peekFront(){
+      return _front.getCargo();
+  }
   //~~~~~~~~~~~~~~~~~~~end AP subset~~~~~~~~~~~~~~~~~~~
 
 
+  public String toString()
+    {
+    String stringified = "";
+    QNode<QUASAR> temp = _front;
+    for (int i = 0; i < _size && _front != null; i++){ // an empty node doesnt count
+      stringified += temp.getCargo() + ", ";
+      temp = temp.getNext();
+    }
+    return stringified;
+    }
 
 
 
 
     /*****************************************************
- * class LLNode
+ * class QNode
  * Implements a node, for use in lists and other container classes.
  * Stores its data as a QUASAR
  *****************************************************/
 
-private class LLNode<QUASAR>
+private class QNode<QUASAR>
 {
     //instance vars
     private QUASAR _cargo;    //cargo may only be of type QUASAR
-    private LLNode _nextNode; //pointer to next LLNode
+    private QNode<QUASAR> _nextNode; //pointer to next QNode
 
     // constructor -- initializes instance vars
-    public LLNode( QUASAR value, LLNode next ) {
+    public QNode( QUASAR value, QNode<QUASAR> next ) {
 	_cargo = value;
 	_nextNode = next;
     }
@@ -45,7 +79,7 @@ private class LLNode<QUASAR>
     //--------------v  ACCESSORS  v--------------
     public QUASAR getCargo() { return _cargo; }
 
-    public LLNode getNext() { return _nextNode; }
+    public QNode<QUASAR> getNext() { return _nextNode; }
     //--------------^  ACCESSORS  ^--------------
 
 
@@ -56,11 +90,37 @@ private class LLNode<QUASAR>
 	return foo;
     }
 
-    public LLNode setNext( LLNode newNext ) {
-	LLNode foo = getNext();
+    public QNode<QUASAR> setNext( QNode<QUASAR> newNext ) {
+	QNode<QUASAR> foo = getNext();
 	_nextNode = newNext;
 	return foo;
     }
 
-}//end class LLNode
+}//end class QNode
+
+/*
+public static void main(String[] args) { // for testing
+    NodeQueue<String> test = new NodeQueue<String>();
+    test.enqueue("hi");
+    System.out.println(test);
+
+    test.enqueue("bye");
+    System.out.println(test);
+
+    test.enqueue("hola");
+    System.out.println(test);
+
+    test.enqueue("adios");
+    System.out.println(test);
+
+    test.dequeue();
+    System.out.println(test);
+
+    test.dequeue();
+    System.out.println(test);
+
+    test.dequeue();
+    System.out.println(test);
+}
+*/
 }
