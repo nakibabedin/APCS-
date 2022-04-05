816fcd381ffac763e59a3b091700f2d43a24ba59
nakibabedin <nabedin30@stuy.edu>
2022-04-05 19:53:56 -0400

    add 100_data

---
+++ 100_data/Cereal.java
@@ -0,0 +1,48 @@
+public class Cereal {
+    String name;
+    double calories;
+    double protein;
+    double fat;
+    double sodium;
+    double fiber;
+    double carbs;
+    double sugar;
+    double potassium;
+    double vitamins;
+    double shelf;
+    double weight;
+    double cups;
+    double ratings;
+
+    public Cereal(
+      String name,
+      double calories,
+      double protein,
+      double fat,
+      double sodium,
+      double fiber,
+      double carbs,
+      double sugar,
+      double potassium,
+      double vitamins,
+      double shelf,
+      double weight,
+      double cups,
+      double ratings)
+      {
+        this.name = name;
+        this.calories = calories;
+        this.protein = protein;
+        this.fat = protein;
+        this.sodium = sodium;
+        this.fiber = fiber;
+        this.carbs = carbs;
+        this.sugar = sugar;
+        this.potassium = potassium;
+        this.vitamins = vitamins;
+        this.shelf = shelf;
+        this.weight = weight;
+        this.cups = cups;
+        this.ratings = ratings;
+    }
+}
